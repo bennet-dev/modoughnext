0c1d04a57d9cc24b49af3350a30ea5ee2f380c88
Twill1007 <85330522+Twill1007@users.noreply.github.com>
2023-09-26 14:40:54 -0700

    CartSlice Update

--- src/state/cart-slice.js
+++ src/state/cart-slice.js
@@ -1,9 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = [];
+const initialState = { cart: [] };
 
 export const cookieSlice = createSlice({
   name: "CookieDough",
   initialState,
-  reducers: {},
+  reducers: {
+    addItemToCart: (state, action) => {
+      const newItem = action.payload;
+      state.cart.push(newItem);
+    },
+    removeItemFromCart: (state, action) => {
+      const itemToRemove = action.payload;
+      state.cart = state.cart.filter((item) => item !== itemToRemove);
+    },
+  },
 });
